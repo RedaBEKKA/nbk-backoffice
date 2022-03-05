bd33d612ce70d967403c52d195e477445b6550ed
salahberiani <salaheddinberiani@gmail.com>
2022-03-05 15:49:14 +0100

    refactor

--- src/pages/wallet/hooks/useGetWallets.js
+++ src/pages/wallet/hooks/useGetWallets.js
@@ -1,7 +1,7 @@
-import { useEffect, useMemo } from 'react';
-import { Badge, Text } from '@chakra-ui/react';
-import useStore from 'store';
-import { Link } from 'react-router-dom';
+import { useEffect, useMemo } from "react";
+import { Badge, Text } from "@chakra-ui/react";
+import useStore from "store";
+import { Link } from "react-router-dom";
 export default function useGetWallets() {
   const getLoading = useStore((state) => state.wallets.getLoading);
   const wallets = useStore((state) => state.wallets.wallets);
@@ -12,46 +12,46 @@ export default function useGetWallets() {
   const walletColumns = useMemo(
     () => [
       {
-        Header: 'id',
-        accessor: 'walletId',
+        Header: "id",
+        accessor: "walletId",
       },
       {
-        Header: 'type id',
-        accessor: 'walletTypeId',
+        Header: "type id",
+        accessor: "walletTypeId",
       },
       {
-        Header: 'user id',
-        accessor: 'userId',
+        Header: "user id",
+        accessor: "userId",
       },
       {
-        Header: 'pay in count',
-        accessor: 'payinCount',
+        Header: "pay in count",
+        accessor: "payinCount",
       },
       {
-        Header: 'pay out count',
-        accessor: 'payoutCount',
+        Header: "pay out count",
+        accessor: "payoutCount",
       },
       {
-        Header: 'currency',
-        accessor: 'currency',
+        Header: "currency",
+        accessor: "currency",
       },
       {
-        Header: 'status',
-        accessor: 'status',
+        Header: "status",
+        accessor: "status",
         Cell: ({ row: { original } }) => {
           return (
             <>
-              {original.walletStatus === 'PENDING' && (
+              {original.walletStatus === "PENDING" && (
                 <Badge variant="solid" colorScheme="orange">
                   en attendant
                 </Badge>
               )}
-              {original.walletStatus === 'VALIDATED' && (
+              {original.walletStatus === "VALIDATED" && (
                 <Badge variant="solid" colorScheme="green">
                   validé
                 </Badge>
               )}
-              {original.walletStatus === 'CANCELED' && (
+              {original.walletStatus === "CANCELED" && (
                 <Badge variant="solid" colorScheme="red">
                   annulé
                 </Badge>
@@ -61,12 +61,14 @@ export default function useGetWallets() {
         },
       },
       {
-        accessor: 'links',
+        accessor: "links",
         Cell: ({ row: { original } }) => {
           return (
             <>
               <Text color="purple.500" fontWeight="bold" cursor="pointer">
-                <Link to={`/transfers?walletId=${original.walletId}`}>transfers</Link>
+                <Link to={`/transactions?walletId=${original.walletId}`}>
+                  transactions
+                </Link>
               </Text>
             </>
           );
