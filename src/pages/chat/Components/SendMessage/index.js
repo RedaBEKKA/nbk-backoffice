16df63408e75077eda6c6b4debc9eccfd51a8ff5
amariche <insigned12@gmail.com>
2022-05-18 16:36:16 +0200

    feat submit + hover

--- src/pages/chat/Components/SendMessage/index.js
+++ src/pages/chat/Components/SendMessage/index.js
@@ -9,49 +9,60 @@ import React, { useState } from "react";
 import { Icon } from "@chakra-ui/react";
 import { BiSend } from "react-icons/bi";
 import useSendMessages from "pages/chat/Hooks/useSendMessage";
+import { useForm } from "react-hook-form";
 
 const ContainerSendMessage = ({ ChannelSelected }) => {
-
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+  } = useForm();
   const [body, setbody] = useState("");
   const { onSubmit } = useSendMessages();
   const submit = () => {
     onSubmit(ChannelSelected.channelId, ChannelSelected.userId, body);
     setTimeout(() => {
-      setbody("")
+      setbody("");
     }, 2000);
   };
-  const handleChange = (event) => setbody(event.target.value)
+  const handleChange = (event) => setbody(event.target.value);
   return (
-    <Flex
-      bg={"#fff"}
-      m="5"
-      pos={"absolute"}
-      bottom="5rem"
-      w="95%"
-      borderRadius={25}
-    >
-      <InputGroup>
-        <Input
-          placeholder="send a message"
-          borderRadius={25}
-          value={body}
-          onChange={handleChange}
-        />
-        <Button style={{ backgroundColor: "transparent" }} onClick={submit}>
-          <InputRightElement
-            pointerEvents="stroke"
-            children={
-              <Icon
-                color="gray.300"
-                as={BiSend}
-                cursor="pointer"
-                boxSize="30px"
-              />
-            }
+    <form onSubmit={handleSubmit(submit)}>
+      <Flex
+        bg={"#fff"}
+        m="5"
+        pos={"absolute"}
+        bottom="5rem"
+        w="95%"
+        borderRadius={25}
+      >
+        <InputGroup>
+          <Input
+            placeholder="send a message"
+            borderRadius={25}
+            value={body}
+            onChange={handleChange}
           />
-        </Button>
-      </InputGroup>
-    </Flex>
+          <Button style={{ backgroundColor: "transparent" }} type="submit">
+            <InputRightElement
+              pointerEvents="stroke"
+              children={
+                <Icon
+                  color="gray.300"
+                  as={BiSend}
+                  cursor="pointer"
+                  boxSize="30px"
+                  _hover={{
+                    color: "#2DDCB1",
+                  }}
+                />
+              }
+            />
+          </Button>
+        </InputGroup>
+      </Flex>
+    </form>
   );
 };
 
